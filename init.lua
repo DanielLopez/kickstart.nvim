9ed45d103e4c5f59b69108cb3731595a627b37a5
Daniel Lopez <thinkboxllc@gmail.com>
2025-02-13 19:16:57 -0500

    Add Plugins

--- init.lua
+++ init.lua
@@ -952,6 +952,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.vim-tmux-navigator',
+  require 'kickstart.plugins.comment',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
