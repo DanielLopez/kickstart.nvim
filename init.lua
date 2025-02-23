8ca2967af6b0bf3c5d20e6f4ad72e590ea9aadc8
DanielLopez <thinkboxllc@gmail.com>
2025-02-23 16:59:21 -0500

    Plugin Updates

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -951,10 +951,14 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.copilot',
+  -- require 'kickstart.plugins.copilot_chat',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.vim-tmux-navigator',
   require 'kickstart.plugins.comment',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.flash',
+  require 'kickstart.plugins.smear',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
